68692677c9d860719280e2336cc6f5af3f522900
Shuhei Yoshida <aneki525@gmail.com>
2020-11-14 21:23:42 +0900

    feed画面：tableに記事情報を表示

--- Qcli/FeedViewController.swift
+++ Qcli/FeedViewController.swift
@@ -6,18 +6,60 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
-class FeedViewController: UIViewController {
+struct ArticleData {
+    var imgURL: String
+    var titleText: String
+    var discriptionText: String
+    var likeNumber: Int
+    var articleURL: String
+}
 
+class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet weak var searchTxextField: UITextField!
+    @IBOutlet weak var articleTableView: UITableView!
     let cornerRadiusValue: CGFloat = 8
+    var dataList: [ArticleData] = []
+    let tableViewCellHeight: CGFloat = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLayout()
         //ログインした状態ならユーザー情報呼び出し
         if self.isLogined() {
             let token = callUserInfo()
             print("トークン読み込みました\(token)")
         }
+        articleTableView.dataSource = self
+        articleTableView.delegate = self
+        getData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = articleTableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath) as! ArticleTableViewCell
+        cell.titleLabel?.text = dataList[indexPath.row].titleText
+        cell.discriptionLabel?.text = dataList[indexPath.row].discriptionText
+        cell.likeLabel?.text = "\(dataList[indexPath.row].likeNumber)like"
+        let url = URL(string: dataList[indexPath.row].imgURL)
+        do {
+            let imageData = try Data(contentsOf: url!)
+            cell.articleIconImage?.image = UIImage(data: imageData)
+        } catch {
+            cell.articleIconImage?.image = UIImage(named: "no-coupon-image.png")
+        }
+        cell.cellSetLayout()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableViewCellHeight
     }
     
     //userInfo呼び出し
@@ -43,4 +85,28 @@ class FeedViewController: UIViewController {
         searchTxextField.layer.cornerRadius = cornerRadiusValue
     }
     
+    func getData() {
+        let url = "https://qiita.com/api/v2/items?page=1&per_page=20"
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                json.forEach {(_, json) in
+                    if let titleData = json["title"].string,
+                       let discriptionData = json["body"].string,
+                       let likeData = json["likes_count"].int,
+                       let imageURL = json["user"]["profile_image_url"].string,
+                       let articleURL = json["url"].string {
+                        let oneData = ArticleData(imgURL: imageURL, titleText: titleData, discriptionText: discriptionData, likeNumber: likeData, articleURL: articleURL)
+                        self.dataList.append(oneData)
+                    }
+                    
+                }
+                self.articleTableView.reloadData()
+                print("tableリロード")
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
