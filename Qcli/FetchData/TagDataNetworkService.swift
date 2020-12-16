92cfd25049d937f43ca58c202fa6a19df55d2722
Shuhei Yoshida <aneki525@gmail.com>
2020-12-16 10:03:13 +0900

    TagDataNetworkService.swift導入

---
+++ Qcli/FetchData/TagDataNetworkService.swift
@@ -0,0 +1,70 @@
+//
+//  TagDataNetworkService.swift
+//  Qcli
+//
+//  Created by 吉田周平 on 2020/12/16.
+//
+
+import Foundation
+
+class TagDataNetworkService {
+    var perPageNumber: Int?
+    var searchDict: [SearchOption:String]?
+    var pageNumber: Int
+    var sortdict: [QueryOption:SortOption]?
+    var userInfo: QiitaUserInfo?
+    
+    init(sortDict: [QueryOption:SortOption]?,
+         userInfo: QiitaUserInfo?) {
+        self.pageNumber = 1
+        self.perPageNumber = 20
+        self.searchDict = nil
+        self.sortdict = sortDict
+        self.userInfo = userInfo
+    }
+    
+    func fetch(success: @escaping ((_ result: [TagModel]?) -> Void),
+               failure: @escaping ((_ error: NSError?) -> Void)) {
+        //↓URLの設定
+        let reqParamModel = RequestParametersModel(
+            dataType: .tag,
+            pageNumber: self.pageNumber,
+            perPageNumber: self.perPageNumber,
+            searchDict: self.searchDict,
+            sortdict: self.sortdict)
+        let urlText = reqParamModel.assembleItemURL(pageNumber: pageNumber)
+        guard let url = URL(string: urlText) else { return }
+        //↑URLの設定
+        let qiitaRequest = QiitaRequest()
+        //↓ヘッダーの設定
+        if let userInfo = self.userInfo {
+            qiitaRequest.headers = [
+                "Authorization": "Bearer " + userInfo.token
+            ]
+        }
+        //↑ヘッダーの設定
+        //↓QittaRequestのメソッドrequestは引数にheadersを設置しなくてもリクエスト時にヘッダー情報を組み込むようにしている
+        qiitaRequest.request(url: url).response { response in
+            guard let data = response.data else {
+                return
+            }
+            //取得したデータを格納
+            guard let exportData = try? JSONDecoder().decode([TagModel].self, from: data) else {
+                print("An error occurred during decoding.")
+                if let exceptionData = try? JSONDecoder().decode(ErrorModel.self, from: data) {
+                    //1日のリクエスト数が上限に達した場合
+                    if let message = exceptionData.message,
+                       let type = exceptionData.type {
+                        print("message: \(message), type: \(type)")
+                    }
+                } else {
+                    print("Failed to get error message.")
+                }
+                
+                failure(response.error as NSError?)
+                return
+            }
+            success(exportData)
+        }
+    }
+}
