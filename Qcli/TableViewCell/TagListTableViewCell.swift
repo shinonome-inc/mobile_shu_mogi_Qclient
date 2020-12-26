25b6820de2ad17a636a34a73afe0d73f17532bf0
Shuhei Yoshida <aneki525@gmail.com>
2020-12-26 09:53:46 +0900

    TagListTableViewCell不要部分削除

--- Qcli/TableViewCell/TagListTableViewCell.swift
+++ Qcli/TableViewCell/TagListTableViewCell.swift
@@ -12,16 +12,6 @@ class TagListTableViewCell: UITableViewCell {
     @IBOutlet weak var tagIconImage: UIImageView!
     @IBOutlet weak var tagTitle: UILabel!
     @IBOutlet weak var tagCount: UILabel!
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
     func cellSetLayout() {
         //アイコン画像を丸くする
