54a977cf19699b8cc9b2f899b2abb326223cd3ee
Shuhei Yoshida <aneki525@gmail.com>
2021-01-01 21:58:15 +0900

    UserListTableViewCell作成  storyboardと紐付け完了

---
+++ Qcli/TableViewCell/UserListTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  UserListTableViewCell.swift
+//  Qcli
+//
+//  Created by 吉田周平 on 2021/01/01.
+//
+
+import UIKit
+
+class UserListTableViewCell: UITableViewCell {
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userIdLabel: UILabel!
+    
+    func setModel(model: UserData) {
+        userNameLabel.text = model.userName
+        userIdLabel.text = "@" + model.userId
+        if let url = URL(string: model.imageUrl) {
+            userImageView.setImage(with: url, completionHandler: { result in
+                switch result {
+                case .success(_): break
+                case .failure(_):
+                    self.userImageView.image = UIImage(named: "no-coupon-image.png")
+                }
+            })
+        }
+    }
+}
