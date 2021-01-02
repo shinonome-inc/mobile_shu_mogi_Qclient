dd29ac59c58fd9b45e64075678f8644fa7524fea
Shuhei Yoshida <aneki525@gmail.com>
2021-01-02 19:57:40 +0900

    UserListリクエストを送り、UI更新

--- Qcli/ViewController/UserListViewController.swift
+++ Qcli/ViewController/UserListViewController.swift
@@ -9,15 +9,76 @@ import UIKit
 
 class UserListViewController: UIViewController {
     
+    @IBOutlet weak var userListTableView: UITableView!
     //ユーザーリストのタイプ
     var userListType: UserListType?
     //ユーザーid
     var userId: String?
     //取得するデータのリスト
     var dataItems = [UserData]()
+    //送信用データ
+    var sendData: UserData?
+    //データリクエストの宣言
+    var userListDataRequest: UserListNetworlService!
+    //ページカウント
+    var pageNumber = 1
+    //リクエストできるか状態かの判定
+    var isNotLoading = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        guard let userId = self.userId else { return }
+        guard let userListType = self.userListType else { return }
+        self.userListDataRequest = UserListNetworlService(userType: userListType, userId: userId)
+        getData(requestUserListData: userListDataRequest)
     }
+    
+    //apiを叩きデータを保存する
+    func getData(requestUserListData: UserListNetworlService) {
+        requestUserListData.fetch(success: { (dataArray) in
+            dataArray?.forEach { (oneUserListData) in
+                if let name = oneUserListData.name,
+                   let id = oneUserListData.id,
+                   let imageURL = oneUserListData.profileImageUrl,
+                   let itemCount = oneUserListData.itemsCount {
+                    let oneData = UserData(imageUrl: imageURL, userName: name, userId: id, itemCount: itemCount)
+                    self.dataItems.append(oneData)
+                } else {
+                    print("ERROR: This data ↓ allocation failed.")
+                    print(oneUserListData)
+                }
+            }
+            self.userListTableView.reloadData()
+            print("👍 Reload the article data")
+            self.isNotLoading = true
+            
+        }, failure: { error in
+            print("Failed to get the article list data.")
+            if let error = error {
+                print(error)
+            }
+            self.isNotLoading = true
+        })
+    }
+}
+
+extension UserListViewController: UITableViewDelegate {
+    
+}
+
+extension UserListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = userListTableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as? UserListTableViewCell else {
+            abort()
+        }
+        let model = dataItems[indexPath.row]
+        cell.setModel(model: model)
+        return cell
+    }
+    
+    
 }
