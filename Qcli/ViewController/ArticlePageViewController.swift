e558e6c27445b1d0c14e7cb60a2faac6e9d32325
Shuhei Yoshida <aneki525@gmail.com>
2021-01-18 21:33:46 +0900

    ArticlePage→ErrorVCに画面を遷移させる機能を追加した

--- Qcli/ViewController/ArticlePageViewController.swift
+++ Qcli/ViewController/ArticlePageViewController.swift
@@ -12,16 +12,55 @@ class ArticlePageViewController: UIViewController {
     
     @IBOutlet weak var webView: WKWebView!
     var urlStr: String!
-    var articleData: ArticleData!
+    var articleData: ArticleData?
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.navigationDelegate = self
+        webViewLoad(articleData: articleData)
+    }
+    
+    func webViewLoad(articleData: ArticleData?) {
         if let articleData = articleData,
            let url = URL(string: articleData.articleURL) {
-            
             let urlRequest = URLRequest(url: url)
             webView.load(urlRequest)
         }
-        
     }
+    
+}
+
+extension ArticlePageViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError: Error) {
+        segueErrorViewController(qiitaError: .connectionError)
+    }
+
+}
+
+extension ArticlePageViewController: ErrorDelegate {
+    func segueErrorViewController(qiitaError: QiitaError) {
+        //Prepare ErrorViewController
+        guard let storyboard = self.storyboard else { abort() }
+        let identifier = ViewControllerIdentifier.error.rawValue
+        let errorViewController = storyboard.instantiateViewController(identifier: identifier) as! ErrorViewController
+        errorViewController.errorDelegate = self
+        //Send property, Segue
+        errorViewController.qiitaError = qiitaError
+        self.present(errorViewController, animated: true, completion: nil)
+    }
+    
+    func backToLoginViewController() {
+        let identifier = ViewControllerIdentifier.login.rawValue
+        if let storyboard = self.storyboard,
+           let navigationController = self.navigationController {
+            let loginViewController = storyboard.instantiateViewController(identifier: identifier) as! LoginViewController
+            navigationController.pushViewController(loginViewController, animated: true)
+        }
+    }
+    
+    func reload() {
+        webViewLoad(articleData: articleData)
+    }
+    
     
 }
