06a0ea49df37b7e334fb67500bc0480fa0d530cf
Shuhei Yoshida <aneki525@gmail.com>
2021-02-11 17:15:21 +0900

    codeを取得する機能を実装

--- Qcli/ViewController/OAuthViewController.swift
+++ Qcli/ViewController/OAuthViewController.swift
@@ -13,6 +13,7 @@ class OAuthViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        webview.navigationDelegate = self
         loadWebView()
         let getToken = RequestParametersCreater(dataType: .getToken).assembleGetTokenURL()
         print("test getToken: \(getToken)")
@@ -20,8 +21,24 @@ class OAuthViewController: UIViewController {
     
     func loadWebView() {
         let oAuthURL = RequestParametersCreater(dataType: .oauth).assembleOAuthURL()
+        print("test oauth: \(oAuthURL)")
         guard let url = URL(string: oAuthURL) else { return }
         let urlRequest = URLRequest(url: url)
+        
         webview.load(urlRequest)
+    }
+}
+
+extension OAuthViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        if let redirectURL = navigationAction.request.url,
+           let code = redirectURL.getCode() {
+            print("code \(code)")
+            
+        }
+        
+        decisionHandler(.allow)
+        
     }
 }
