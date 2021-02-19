37ad190b9b90247b463459b76067202c87b10ab3
Shuhei Yoshida <aneki525@gmail.com>
2021-02-19 19:01:46 +0900

    NavigationBarのbackgroundColorを変更する

--- Qcli/ViewController/MyPageViewController.swift
+++ Qcli/ViewController/MyPageViewController.swift
@@ -51,6 +51,7 @@ class MyPageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController!.setNavigationBarColor()
         keychain.errorDelegate = self
         hideUserItems()
         setProfile()
