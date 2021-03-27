45ddf7443ad7f57d1d2a79e1106fcc26bad79573
Shuhei Yoshida <aneki525@gmail.com>
2021-03-27 19:27:28 +0900

    ログイン状態ではログイン画面をスキップ

--- Qcli/SceneDelegate.swift
+++ Qcli/SceneDelegate.swift
@@ -16,7 +16,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if let isLogined = UserDefaults.standard.object(forKey: "isLogined") as? Bool {
+            if isLogined {
+                guard let scene = (scene as? UIWindowScene) else { return }
+                let window = UIWindow(windowScene: scene)
+                self.window = window
+                window.makeKeyAndVisible()
+                let vc = UIStoryboard(name: "Main", bundle: nil)
+                    .instantiateViewController(identifier: ViewControllerIdentifier.feed.rawValue) as! FeedViewController
+                let navigationController = UINavigationController(rootViewController: vc)
+                window.rootViewController = navigationController
+            }
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
