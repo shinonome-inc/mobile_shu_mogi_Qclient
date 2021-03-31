8b86cf55cb8e414df4766b3f69a114038422e819
Shuhei Yoshida <aneki525@gmail.com>
2021-03-31 14:12:54 +0900

    ネスト1つ分削除

--- Qcli/SceneDelegate.swift
+++ Qcli/SceneDelegate.swift
@@ -17,15 +17,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
-        if let isLogined = UserDefaults.standard.object(forKey: "isLogined") as? Bool {
-            if isLogined {
-                guard let scene = (scene as? UIWindowScene) else { return }
-                let window = UIWindow(windowScene: scene)
-                self.window = window
-                window.makeKeyAndVisible()
-                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TabView")
-                window.rootViewController = vc
-            }
+        if let isLogined = UserDefaults.standard.object(forKey: "isLogined") as? Bool,
+           isLogined {
+            guard let scene = (scene as? UIWindowScene) else { return }
+            let window = UIWindow(windowScene: scene)
+            self.window = window
+            window.makeKeyAndVisible()
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TabView")
+            window.rootViewController = vc
         }
     }
 
